8b262251ecfa82ef8ea65128a91f452a2af8350c
Gerrof <gerrof@protonmail.ch>
2021-06-24 08:40:29 +0200

    Befehle in Sektionen geteilt, apt auskommentiert und pacman vorbereitet

--- setup.sh
+++ setup.sh
@@ -1,16 +1,30 @@
 #! /bin/bash
-sudo apt-get update -y
-sudo apt-get install bluez bluez-tools -y
-sudo apt-get install bluez-firmware python-bluez python-dev python-pip -y
-sudo pip install evdev
-sudo apt install git python python3 python-dev python3-dev python3-dbus python3-pyudev python3-evdev -y
-sudo apt-get install python-dbus  -y
-sudo apt-get install tmux -y
+
+# Python3 section
+sudo pip3 install evdev
+sudo pip3 install PyBluez
+
+# Archlinux/Manjaro or pacman section
+#sudo pacman -Syu
+#sudo pacman -Syu
+
+# Ubuntu/Debian or APT section
+#sudo apt-get update -y
+#sudo apt-get install \
+	bluez bluez-tools bluez-firmware \
+	python-bluez python-dev python-pip \
+	git python python3 \
+	python-dev python3-dev python3-dbus \
+	python3-pyudev python3-evdev \
+	python-dbus tmux python3-gi \
+	python3-pip \
+	-y
+
+# Simple copy section
 sudo cp dbus/org.thanhle.btkbservice.conf /etc/dbus-1/system.d
 sudo cp /lib/systemd/system/bluetooth.service ./bluetooth.service.bk
 sudo cp bluetooth.service /lib/systemd/system/bluetooth.service
+
+# Post install section
 sudo systemctl daemon-reload
-sudo /etc/init.d/bluetooth start
-sudo apt-get install python3-gi -y
-sudo apt install python3-pip -y
-sudo pip3 install PyBluez
+sudo systemctl start bluetooth
